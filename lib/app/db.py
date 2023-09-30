b89f2ac0c1b8613a06603bc80e8c60594374589c
mark_kibo <kibochamark@gmail.com>
2023-09-30 12:37:35 +0300

    completed assignment

---
+++ lib/app/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer
+
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
